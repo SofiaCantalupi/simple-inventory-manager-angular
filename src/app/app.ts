1c2468687b240b8d38435225cd2df3ca09339da1
SofiaCantalupi <sofiacantalupi@gmail.com>
2025-10-23 15:51:16 -0300

    add  imports at app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,20 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header';
+import { ProductForm } from './components/product-form/product-form';
+import { ProductList } from "./components/product-list/product-list";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, 
+    Header, 
+    ProductForm, 
+    ProductList],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('angular-practice-inventory');
+
+
 }
