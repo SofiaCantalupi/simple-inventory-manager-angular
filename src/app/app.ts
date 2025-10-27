05e9ac0765041a8a95659ed82d32fe55072e0731
SofiaCantalupi <sofiacantalupi@gmail.com>
2025-10-26 22:47:50 -0300

    remove unused product form and list components from app module

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
-import { ProductForm } from './components/product-form/product-form';
-import { ProductList } from "./components/product-list/product-list";
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, 
-    Header, 
-    ProductForm, 
-    ProductList],
+    Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
