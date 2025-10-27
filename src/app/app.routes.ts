1cdab56705370e0a8ac255755e472a9b5aa3ad13
SofiaCantalupi <sofiacantalupi@gmail.com>
2025-10-26 22:47:34 -0300

    define application routes for Home, Inventory, Suppliers, Categories, and ProductDetail

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,33 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Inventory } from './pages/inventory/inventory';
+import { Suppliers } from './pages/suppliers/suppliers';
+import { Categories } from './pages/categories/categories';
+import { ProductDetail } from './pages/product-detail/product-detail';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: Home
+    },
+    {
+        path: 'inventory',
+        component: Inventory
+    },
+    {
+        path: 'suppliers',
+        component: Suppliers
+    },
+    {
+        path: 'categories',
+        component: Categories
+    },
+    {
+        path: 'product/:id',
+        component: ProductDetail
+    },
+    {
+        path: '**',
+        redirectTo: ''
+    }
+];
