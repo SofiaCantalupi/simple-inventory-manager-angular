a3e93950f93502fe8e3d22e8745fbecfa9d3ac54
SofiaCantalupi <sofiacantalupi@gmail.com>
2025-10-26 22:47:14 -0300

    improve HTML structure and include RouterLink in imports

--- src/app/components/product-list/product-list.ts
+++ src/app/components/product-list/product-list.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { ProductService } from '../../service/product-service';
 import { ProductCard } from '../product-card/product-card';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-product-list',
-  imports: [ProductCard],
+  imports: [ProductCard, RouterLink],
   templateUrl: './product-list.html',
   styleUrl: './product-list.css'
 })
