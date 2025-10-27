9cba50e15056131ea584b6eabcfe0d3d1edefe5b
SofiaCantalupi <sofiacantalupi@gmail.com>
2025-10-27 11:01:46 -0300

    remove unused imports from header and product list components

--- src/app/components/product-list/product-list.ts
+++ src/app/components/product-list/product-list.ts
@@ -5,7 +5,7 @@ import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-product-list',
-  imports: [ProductCard, RouterLink],
+  imports: [RouterLink],
   templateUrl: './product-list.html',
   styleUrl: './product-list.css'
 })
